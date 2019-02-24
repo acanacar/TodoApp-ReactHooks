c722cf26520352cd7f8c3024aeb505aaf8e8070b
acanacar <ahmetcanacar.1@gmail.com>
2019-02-24 12:47:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function Todo({ todo, index }) {
+  return (
+    <div className={'todo'}>{todo.text}</div>
+  )
 }
 
-export default App;
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('')
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (!value) return
+    addTodo(value)
+    setValue('')
+
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type={'text'}
+        className={'input'}
+        placeholder={'Add Todo..'}
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+
+  )
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    { text: 'hello 1 todo', isCompleted: false },
+    { text: 'hello 2 todo', isCompleted: false },
+    { text: 'hello 3 todo', isCompleted: false },
+  ])
+  const addTodo = text => {
+    const newTodo = [...todos, { text }]
+    setTodos(newTodo)
+  }
+  return (
+    <div className={'app'}>
+      <div className={'todo-list'}>
+        {todos.map((todo, index) => (<Todo key={index} index={index} todo={todo} />))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  )
+}
+
+export default App
